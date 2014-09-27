c3d482dbfd4affd657a54960bb32f4610c92e1cc
halukugurlu <halukugurlu@gmail.com>
2014-09-27 02:20:50 -0400

    index.html added, js code modified to demonstrate in the markup

--- ArrayDuplicateElementChecker/Array.js
+++ ArrayDuplicateElementChecker/Array.js
@@ -2,20 +2,25 @@
  * Created by halukugurlu on 9/14/14.
  * This document illustrates how to find duplicate values in an array.
  */
-
 var myArray = [3, 2, 2, 82, 1, 33, 82, 3, 64, 23, 15, 1, 64];
+document.getElementById("test").innerHTML = myArray;
+
+function myFunction(){
+
 
 var sortedArray = myArray.sort(function(a, b) {     //  The array elements are sorted in numerically
                                  return a - b;      //  in order to compare indexes that contain
                                });                  //  the same values.
 
-console.log(sortedArray); // The sorted index of the Array
+//console.log(sortedArray); // The sorted index of the Array
 
 var results = [];
 for (var i = 0; i < sortedArray.length; i++) {
     if (sortedArray[i] == sortedArray[i + 1]) {
         results.push(sortedArray[i]);
+        document.getElementById("test").innerHTML = results;
     }
 }
 
-console.log(results); //  The duplicate values in the Array -> 1 , 2 , 3 , 64 , 82
+//console.log(results); //  The duplicate values in the Array -> 1 , 2 , 3 , 64 , 82
+}
